cb82edd6ad0b9571e460f70aaeb0d3fd13b535eb
Yuichi Hayashi <yuichi_hayashi_0506@outlook.jp>
2026-08-04 05:36:33 +0900

    feat: コンタクトCRUD実装

---
+++ app/Http/Requests/StoreContactRequest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreContactRequest extends FormRequest
+{
+    /**
+     * リクエストの認可 ※誰でも許可するため一律True
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * バリデーションルール
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        // dd($this->all());
+
+        if ($this->input('action') === 'back') {
+            return [];
+        }
+
+        return [
+            'category_id' => 'required|exists:categories,id',
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'gender' => 'required|integer|in:1,2,3',
+            'email' => 'required|string|email|max:255',
+            'tel' => ['required', 'string', 'regex:/^[0-9]{10,11}$/'],
+            'tel1' => ['required', 'string'],
+            'tel2' => ['required', 'string'],
+            'tel3' => ['required', 'string'],
+            'address' => 'required|string|max:255',
+            'building' => 'nullable|string|max:255',
+            'detail' => 'nullable|string|max:120',
+            'tag_ids' => 'nullable|array',
+            'tag_ids.*' => 'exists:tags,id',
+        ];
+    }
+
+    /**
+     * バリデーションメッセージ
+     */
+    public function messages(): array
+    {
+        return [
+            'first_name.required' => '姓を入力してください',
+            'last_name.required' => '名を入力してください',
+            'gender.required' => '性別を入力してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'tel.required' => '電話番号を入力してください',
+            'address.required' => '住所を入力してください',
+            'category_id.required' => 'お問い合わせの種類を選択してください',
+            'detail.required' => 'お問い合わせ内容を入力してください',
+            'detail.max' => 'お問い合わせ内容は120文字以内で入力してください',
+        ];
+    }
+}
